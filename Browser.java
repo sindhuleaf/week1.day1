a4898c8165330b39080b7857bb2d1935312b446a
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-10 10:12:50 +0530

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,7 @@
+package week1.day1;
+
+public class Browser {
+public static void main(String[] args) {
+	System.out.println("this is my first script");
+}
+}
---
+++ Mobile.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Mobile {
+public static void makecall() {
+	System.out.println("give a ring");
+}
+public static void sendmessage() {
+	System.out.println("ping me");
+}
+public static void main(String[] args) {
+	System.err.println("this is my mobile");
+	makecall();
+	sendmessage();
+}
+}
+
