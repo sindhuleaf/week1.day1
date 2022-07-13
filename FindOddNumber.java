04be4d4d3533315ac9e9d825a796fb9b28addf4d
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-13 17:49:05 +0530

    Add files via upload

---
+++ FindOddNumber.java
@@ -0,0 +1,17 @@
+package week1.assignment;
+
+public class FindOddNumber {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("printing only the odd numbers from 1 to 20");
+		   for(int i=1;i<=20;i++)
+		{
+			if(i%2!=0)
+			{
+			System.out.println(i);
+			}
+
+	}
+
+}
+}
