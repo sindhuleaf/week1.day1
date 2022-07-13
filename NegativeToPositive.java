2319977947b37386965b97dbc1ecd5f0775532eb
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-13 18:01:20 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,15 @@
+package week1.assignment;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=-40;
+		int a=-(i);
+		if(a>0)
+		System.out.println(i + " is converted to positive number " + a);
+		else
+		System.out.println(a + " is negative number");
+	}
+
+}
