50d8e98d9c6f03533aa8f7fe158128263ff99a5e
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-13 17:58:48 +0530

    Add files via upload

---
+++ PositiveorNegative.java
@@ -0,0 +1,22 @@
+package week1.assignment;
+
+public class PositiveorNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		if(i<0) 
+		{
+		System.out.println(i + " is negative number");
+		}
+		if(i>0) 
+		{
+		System.out.println(i + " is positive number");
+		}
+		else
+		{
+		System.out.println(i + " is neither positive nor negative number");
+		}
+	}
+
+}
