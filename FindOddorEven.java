f7fa42ca350d714a5d470b69b40328b6c2952449
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-13 17:51:39 +0530

    Add files via upload

---
+++ FindOddorEven.java
@@ -0,0 +1,20 @@
+package week1.assignment;
+
+public class FindOddorEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=25;
+		if (i % 2==0)
+		{
+		System.out.println(i + " is even number");
+		} 
+		else
+		{
+        System.out.println(i + " is odd number");
+		}
+
+	}
+
+}
