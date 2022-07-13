cdaba128f97192d37223974c6c4d9b64d087ecf4
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-13 17:42:35 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+package week1.assignment;
+
+public class Calculator {
+	
+	  int add(int num1,int num2,int num3) {
+			return(num1+num2+num3);
+		}
+	        int sub(int num1,int num2) {
+			return(num1-num2);
+		}
+	        double mul(double num1,double num2) {
+		    return(num1*num2);
+	}
+	        float div(float num1,float num2) {
+		    return(num1/num2);
+	}
+	        public static void main(String[] args) {       
+}
+}
